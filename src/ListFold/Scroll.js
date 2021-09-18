23b0650487c94407a8099b71c623d211a0471ef6
JackieLee <nyjackielee@163.com>
2021-09-18 17:59:29 +0800

    5.59

--- src/ListFold/Scroll.js
+++ src/ListFold/Scroll.js
@@ -9,18 +9,7 @@ const Scroll = () => {
     <div>    
 
 
-      <motion.h1
-        style={{
-          padding: 30,
-          color: "#333",
-          marginLeft: 90,
-          y: 0
-        }}
 
-        transition={{ type: "spring", velocity: 120 }}
-      >
-        横滑定位
-      </motion.h1>
       <Link to="/List">
         <img  blurRadius={100}  src={backIcon} alt=" " style={{
           width: 30,
@@ -49,32 +38,31 @@ const Scroll = () => {
           }}
         >
         </div>
-        <div  className="BlurCard" 
 
-          style={{
-         
-            backgroundColor: "#ccc",
-            width: 375,
-            height: 812,
-            position: 'absolute',
-            top: 0,
-            opacity: 0.6,
-            zIndex: '10'
-          }}
-        >
-        </div>
+<div className="blackMask"
+ style={{
+         backgroundColor:"#000000",
+          width: 375,
+          height: 812,
+          position: 'absolute',
+          top: 0,
+          opacity: 0.4,
+          zIndex: '2'
+        }}
+></div>
         <div className="mask"
          style={{
           overflow:'hidden',
           top:0,
+          color:"#000",
+         
           width: 375,
           height: 812,
           position: 'absolute',
        
         }}
-        
-        
         >
+
         <div className="BlurBackground"
         style={{
           backgroundImage: `url(${background})`,
