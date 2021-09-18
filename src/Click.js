7513e4ec792dc822f53df49651ff78b682f92514
JackieLee <nyjackielee@163.com>
2021-09-18 10:52:02 +0800

    变更背景

--- src/Click.js
+++ src/Click.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import background from "./bg.png";
+import background from "./bg2.png";
 import backIcon from "./Icon/back.png";
 import { Link } from 'react-router-dom';
 
