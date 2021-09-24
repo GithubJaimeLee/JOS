d941373feab7a4bcfeb67da78637ec16fd4b1510
JackieLee <nyjackielee@163.com>
2021-09-24 18:42:59 +0800

    09241842

--- src/DeskFold/deskChange.js
+++ src/DeskFold/deskChange.js
@@ -1,5 +1,5 @@
 // color: "#415FFF"
-import { animate, motion, useCycle } from 'framer-motion';
+import { motion, useCycle, AnimateSharedLayout, AnimatePresence } from 'framer-motion';
 import * as React from "react";
 /* import background from "../bg2.png"; */
 import zoomIcon from "../Icon/zoomIcon.png";
@@ -8,7 +8,7 @@ import { Grid } from "antd-mobile";
 import backIcon from "../Icon/back.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
-import $ from "jquery";
+
 //press功能
 
 
@@ -28,14 +28,7 @@ const boxHome = {
     top: 60
 }
 
-/* gridRowStart: `span ${rowSpan[url] || 1}`,
-gridColumnStart: `span ${colSpan[url] || 1}`,
-boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.05)", 
-backgroundSize: "cover",
-backgroundPosition: "center",
-backgroundColor: "#fff",
-borderRadius: 12,
-...style */
+
 
 const boxAnimation = {
     animationOne: {
@@ -56,64 +49,18 @@ const boxAnimation = {
     }
 }
 
-/* 
-const btnAnimation = {
-    animationOneBtn: {
-        width: 70,
-        height: 70
-    },
-    animationTwoBtn: {
-        width: 140,
-        height: 70
-    },animationThreeBtn: {
-        width: 140,
-        height: 70
-    }
-} */
-//--------------------------------新增开始--------------------------------------------
-/* const longPressEvents = (callback, ms = 500) => {
 
-    let timeout = null;
 
-    const start = () => (timeout = setTimeout(callback, ms));
-    const stop = () => timeout && window.clearTimeout(timeout);
 
-    return callback
-        ? {
-            onMouseDown: start,
-            onMouseUp: stop,
-            onMouseLeave: stop,
-            onTouchStart: start,
-            onTouchMove: stop,
-            onTouchEnd: stop
-        }
-        : {};
-};
- */
-/*--------------------------------新增结束--------------------------------------------*/
-/* $(function() { $("div.pressBtn").on("Click",function(){
-    $(this).style({"display":"none"});
-        }); */
 const DChange = () => {
     const [animationBox, cycleAnimation] = useCycle("animationOne", "animationTwo", "animationThree", "animationFour");
-   /*  const [animationBtn, cycleAnimation] = useCycle("animationOneBtn", "animationTwoBtn","animationThreeBtn"); */
-    //--------------------------------新增开始--------------------------------------------
-   /*  const [style, setStyle] = useState("block");
-    const check = () => setStyle("none"); */
-    //--------------------------------新增结束--------------------------------------------
- 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = () => setIsOpen(!isOpen);
 
- 
+
     return (
-
+        
         <div>
-            { /*--------------------------------新增开始-------------------------------------------
-               <div className="App">
-
-                    <h2>{style}</h2>
-                </div>
-           --------------------------------新增结束--------------------------------------------*/}
-
             <Link to="/Desk">
                 <img src={backIcon} alt=" " style={{
                     width: 30,
@@ -129,19 +76,17 @@ const DChange = () => {
                 display: Grid,
                 marginTop: 60
             }}
-            
+
             >
                 <motion.div className='boxChange'
                     style={boxHome}
                     variants={boxAnimation}
                     animate={animationBox}
-   //                 {...longPressEvents(check)}
-                
+                  onClick={toggleOpen}
                 >
 
                     <p
                         style={{
-
                             color: "#666",
                             position: 'absolute',
                             top: 10,
@@ -149,45 +94,44 @@ const DChange = () => {
                             fontSize: 14
                         }}
                     >Press me</p>
-                    <div onClick={() => cycleAnimation()} >
-                        <img src={zoomIcon} alt=" " style={{
-                            width: 16,
-                            position: 'absolute',
-                            bottom: 2,
-                            right: 2,
-                            opacity: 1,
-                            zIndex: '100'
-                        }}></img>
-                        <motion.div className="boxPress"
-                //         variants={btnAnimation}
-                //          animate={animationBtn}
-             
-                            style={
-                                {
-                                    display: 'block',
-                                    position: 'absolute',
-                                    right: -10,
-                                    bottom: -10,
-                                    width: 40,
-                                    height: 40,
-                                    backgroundColor: "#fff",
-                                    borderRadius: 20,
-                                    textAlign: 'center',
-                                    boxShadow: "0px 0px 8px 0px rgba(0, 0, 0, 0.3)"
-                                }
-                            }>
-                        </motion.div>
-                    </div>
+                    <AnimatePresence>{isOpen &&
+                    
+                        <div className='CPbtn' onClick={() => cycleAnimation()} >
+            <img src={zoomIcon} alt=" " style={{
+                width: 16,
+                position: 'absolute',
+                bottom: 2,
+                right: 2,
+                opacity: 1,
+                zIndex: '100'
+            }}></img>
+            <motion.div className="boxPress"
+            
+                //exit={{ opacity: 0 }}
+                style={
+                    {
+                        display: 'block',
+                        position: 'absolute',
+                        right: -10,
+                        bottom: -10,
+                        width: 40,
+                        height: 40,
+                        backgroundColor: "#fff",
+                        borderRadius: 20,
+                        textAlign: 'center',
+                        boxShadow: "0px 0px 8px 0px rgba(0, 0, 0, 0.3)"
+                    }
+                }>
+            </motion.div>
+        </div>
+                    
+                    
+                    }</AnimatePresence>
                 </motion.div>
             </div>
            
-           
-           
-           
-           
-            <div style={{ marginLeft: 180, marginTop: -116 }}>
-            </div>
-            <div style={{
+        
+            <div className='Background' style={{
                 background: '#E6ECF4',
                 width: 375,
                 height: 812,
