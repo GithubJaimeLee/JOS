0458e1cfc322e0751b846c08d18435a661bb250b
JackieLee <nyjackielee@163.com>
2021-09-26 11:28:23 +0800

    09261128

--- src/DeskFold/deskChange.js
+++ src/DeskFold/deskChange.js
@@ -25,7 +25,8 @@ const boxHome = {
     borderRadius: 12,
     position: 'absolute',
     left: 20,
-    top: 60
+    top: 60,
+    zIndex: 20
 }
 
 
@@ -55,14 +56,19 @@ const boxAnimation = {
 const DChange = () => {
     const [animationBox, cycleAnimation] = useCycle("animationOne", "animationTwo", "animationThree", "animationFour");
     const [isOpen, setIsOpen] = useState(false);
-   // const toggleOpen = () => setIsOpen(!isOpen);
+    // const toggleOpen = () => setIsOpen(!isOpen);
     const toggleOpen = () => setIsOpen(true);
-    
-
-
+    //  const toggleClose = () => setIsOpen(!isOpen); 
+    // const [isClose, setIsClose] = useState(true);
+    // const toggleOpen = () => setIsOpen(!isOpen);
+    const toggleClose = () => setIsOpen(false);
+    // const [isClose, setIsClose] = useState(false);
+    // const toggleClose = () => setIsOpen(!isOpen); 
+    // const toggleOpen = () => setIsOpen(!isOpen);
+    //   const toggleOpen = () => setIsOpen(true);
 
     return (
-        
+
         <div>
             <Link to="/Desk">
                 <img src={backIcon} alt=" " style={{
@@ -75,19 +81,34 @@ const DChange = () => {
                 }}>
                 </img>
             </Link>
-            <div className='boxAll' style={{
+            {/*   grid 开始          <div className='boxAll' style={{
                 display: Grid,
                 marginTop: 60
             }}
+   > </div>*/}
 
-            >
+            <AnimatePresence>
+                {/*--------------------------------      卡片元素包括按钮  开始   ---------------------------*/}
+
+                {/*--------------------------------     背景元素  开始   ---------------------------*/}
+                <div className='Background'
+                    style={{
+                        backgroundColor: '#E6ECF4',
+                        position: 'absolute',
+                        width: 375,
+                        height: 812,
+                        zIndex: 10,
+                    }}
+                    onClick={toggleClose} >
+
+                </div>
+                {/*--------------------------------      背景  结束    --------------------------*/}
                 <motion.div className='boxChange'
                     style={boxHome}
                     variants={boxAnimation}
                     animate={animationBox}
                     onClick={toggleOpen}
                 >
-
                     <p
                         style={{
                             color: "#666",
@@ -97,53 +118,42 @@ const DChange = () => {
                             fontSize: 14
                         }}
                     >Press me</p>
-                    <AnimatePresence>{isOpen &&
-                    
-                        <div className='CPbtn' onClick={() => cycleAnimation()} >
-            <img src={zoomIcon} alt=" " style={{
-                width: 16,
-                position: 'absolute',
-                bottom: 2,
-                right: 2,
-                opacity: 1,
-                zIndex: '100'
-            }}></img>
-            <motion.div className="boxPress"
-            
-                //exit={{ opacity: 0 }}
-                style={
-                    {
-                        display: 'block',
-                        position: 'absolute',
-                        right: -10,
-                        bottom: -10,
-                        width: 40,
-                        height: 40,
-                        backgroundColor: "#fff",
-                        borderRadius: 20,
-                        textAlign: 'center',
-                        boxShadow: "0px 0px 8px 0px rgba(0, 0, 0, 0.3)"
+
+                    {/*--------------------------------     整个按钮元素  开始   ---------------------------*/}
+                    {isOpen &&
+                        <div className='Allbtn' onClick={() => cycleAnimation()} >
+                            <img src={zoomIcon} alt=" " style={{
+                                width: 16,
+                                position: 'absolute',
+                                bottom: 2,
+                                right: 2,
+                                opacity: 1,
+                                zIndex: '100'
+                            }}></img>
+                            <div className="boxPress"
+                                //exit={{ opacity: 0 }}
+                                style={
+                                    {
+                                        display: 'block',
+                                        position: 'absolute',
+                                        right: -10,
+                                        bottom: -10,
+                                        width: 40,
+                                        height: 40,
+                                        backgroundColor: "#fff",
+                                        borderRadius: 20,
+                                        textAlign: 'center',
+                                        boxShadow: "0px 0px 8px 0px rgba(0, 0, 0, 0.3)"
+                                    }
+                                }>
+                            </div>
+                        </div>
                     }
-                }>
-            </motion.div>
-        </div>
-                    
-                    
-                    }</AnimatePresence>
+                    {/*--------------------------------     整个按钮元素 结束   ---------------------------*/}
                 </motion.div>
-            </div>
-           
-        
-            <div className='Background' style={{
-                background: '#E6ECF4',
-                width: 375,
-                height: 812,
-                position: 'absolute',
-                top: 0,
-                opacity: 1,
-                zIndex: '-1'
-            }}>
-            </div>
+                {/*--------------------------------      卡片元素包括按钮  结束    ---------------------------*/}
+            </AnimatePresence>
+
         </div>
     );
 }
