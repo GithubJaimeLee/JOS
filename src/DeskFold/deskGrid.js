867e62881a90aaf7fb7d02e774bf076db5e11d7f
JackieLee <nyjackielee@163.com>
2021-09-27 18:49:10 +0800

    09271849

--- src/DeskFold/deskGrid.js
+++ src/DeskFold/deskGrid.js
@@ -1,49 +1,94 @@
-import backIcon from "../Icon/back.png";
-import { Link } from 'react-router-dom';
-import React  from 'react';
 
 
-/* //import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
-import { createSnapModifier } from '@dnd-kit/modifiers';
-//import {Draggable} from './Draggable';s
-//import {Droppable} from './Droppable';
+//import "./styles.css";
 
+// currently attempting a draggable iframe for a picture-in-picture video instead of just an img
+// https://codesandbox.io/s/framer-motion-pip-iframe-1kl17
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 
-import {DndContext, DragOverlay} from  '@dnd-kit/core';
-import {
-  restrictToVerticalAxis,
-  restrictToWindowEdges,
-} from '@dnd-kit/modifiers';
- */
+const Pip = ({ appRef }) => {
+    const pipRef = useRef(null);
 
-import { fabric } from "fabric";
+    const modifyTarget = (target) => {
+        if (pipRef.current && appRef.current) {
+            const constraintsRect = appRef.current.getBoundingClientRect();
+            const pipRect = pipRef.current.getBoundingClientRect();
+            const pipMiddleX = pipRect.width / 2;
+            const pipMiddleY = pipRect.height / 2;
 
+            if (target + pipMiddleX > constraintsRect.width / 2) {
+                return constraintsRect.width;
+            } else if (target + pipMiddleY > constraintsRect.height / 2) {
+                return constraintsRect.height;
+            }
 
+            return 0;
+        }
+    };
 
-const DGrid = () => {
-  
+    const transition = {
+        power: 0,
+        min: 0,
+        max: 200,
+        timeConstant: 250,
+        modifyTarget
+    };
 
     return (
+        <motion.div
+            className="pip"
+            ref={pipRef}
+            drag
+            dragConstraints={appRef}
+            dragElastic={0.1}
+            dragTransition={transition}
+            whileTap={{ cursor: "grabbing" }}
+        >
+            <div
 
-        <>
-    
-        <Link to="/Desk">
-          <img src={backIcon} alt=" " style={{
-            width: 30,
-            position: 'absolute',
-            top: 20,
-            left: 20,
-            opacity: 1,
-            zIndex: '100'
-          }}>
-          </img>
-        </Link>
-   
-        </>
+                style={{
+                    width: 80,
+                    height: 100,
+                    backgroundColor: '#000'
+                }}
+
+            />
+        </motion.div>
     );
-  }
+};
 
 
-  export default DGrid;
 
-  
+export default function App() {
+    const appRef = React.useRef(null);
+
+    return (
+        <div className="App"
+            style={{
+                width: 375,
+                height: 812,
+                position: "relative",
+                display: "grid",
+                gridTemplateColumns: `repeat(1fr, 1fr)`,
+                gridAutoRows: `repeat(1fr, 1fr)`
+            }}
+            ref={appRef}>
+            <Pip
+                style={{
+                    top: 0,
+                    left: 0,
+                    width: 100,
+                    height: 100,
+                    position: "absolute"
+                }}
+
+
+
+
+
+
+                appRef={appRef} />
+        </div>
+    );
+}
