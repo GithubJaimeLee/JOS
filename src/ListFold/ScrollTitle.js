8e76964b5ea0ebb1c483fc33b3aea8e64aaed26c
JackieLee <nyjackielee@163.com>
2021-10-06 04:21:23 +0800

    100062

--- src/ListFold/ScrollTitle.js
+++ src/ListFold/ScrollTitle.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 //import { useRef } from "react";
-import { motion} from 'framer-motion';
+//import { motion} from 'framer-motion';
 //import background from "../bg2.png";
 /* import backIcon from "../Icon/back.png"; */
 import Bback from "../Icon/back.png";
@@ -8,78 +8,96 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 import WSetHeader from "../Img/WSetHeader.png";
 import SetBody from "../Img/SetBody.png";
+//import { Frame, Scroll } from "framer";
+import '../scroll.css';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+
+//new
+
+//import { motion, useViewportScroll, useTransform, useMotionValue } from "framer-motion";
 
 
 const ScrollTitle = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+//new
+/*   const { scrollYProgress } = useViewportScroll();
+const scale = useTransform(scrollYProgress, [0, 1], [0, 1]); */
+/* const y = useMotionValue(0)
+const cardy = useTransform(y, value => value /-1)
+const bgy = useTransform(y, value => value /2) */
   return (
     <div>
-
-
-
-      <Link to="/List">
+        <Link to="/List">
         <img  src={Bback} alt=" " style={{
           width: 30,
-          position: 'absolute',
+          position: 'fixed',
           top: 20,
           left: 20,
           opacity: 1,
-          zIndex: '100'
+          zIndex: '10000'
         }} />
       </Link>
-          <motion.div className="HeadCard"
-          drag="y"
-          dragConstraints={{ top: -40, bottom: 0 }}
-            dragElastic={0}
-            style={{    
-              backgroundColor: '#bbb',
-              backgroundImage: `url(${WSetHeader})`, 
-              top:0, 
-              width: 375,
-              height: 148,
-              position: 'absolute',
-          
-              opacity: 1,
-              zIndex: 1
-            }}>   <p
-            style={{
-            top:50,
-            left:20,
-            position:'absolute',
-            fontSize:26,
-            fontWeight:600
-            }}
-            >设置</p>
-          </motion.div>
+    <div className="wrapper">
+      <motion.div
+        className="container"
+        style={{
+          scale
+        }}
+      >
+        <motion.div
+          className="item"
+          style={{
+            scaleY: scrollYProgress
+          }}
+        />
+      </motion.div>
+    </div>
+  {/*  <Scroll 
+      style={{
+    width:70,
+     height: 200,
+     backgroundColor: '#FFFFFF',
+     zIndex:700
+     }}
+     >
 
+      <Frame
+   style={{
+    width:70,
+     height: 70,
+     backgroundColor: '#000000',
+     zIndex:800
+   }}/>
+         <Frame
+   style={{
+    width:70,
+     height: 70,
+     backgroundColor: '#000000',
+     zIndex:800
+   }}/>
+         <Frame
+   style={{
+    width:70,
+     height: 70,
+     backgroundColor: '#000000',
+     zIndex:800
+   }}/>
+   </Scroll> */}
 
-          <motion.div className="MoveCard"
-          drag="y"
-          dragConstraints={{ top: -900, bottom: 60 }}
-            dragElastic={0}
-            dragTransition={{ bounceStiffness: 176, bounceDamping: 26 }}
-            style={{    
-              backgroundColor: '#ddd',
-              backgroundImage: `url(${SetBody})`, 
-              top:88, 
-              width: 375,
-              height: 1590,
-              position: 'absolute',
-              opacity: 1,
-              zIndex: 0
-            }}>
-          </motion.div>
+     {/* ß */}
           
-                      <div className="Background"
+           {/*            <div className="Background"
               style={{
-                backgroundColor: '#f7f7f7',
+                backgroundColor: '#000',
                 width: 375,
                 height: 812,
                 position: 'absolute',
                 top: 0,
                 left:0,
                 opacity: 1,
-                zIndex: -1
-              }} ></div>
+                zIndex: -1000
+              }} ></div> */}
           </div>
 
   );
