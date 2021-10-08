461a10be37d8651da8515dfc43d7947988781797
JackieLee <nyjackielee@163.com>
2021-10-08 15:04:26 +0800

    10081504

--- src/ListFold/ScrollTitle.js
+++ src/ListFold/ScrollTitle.js
@@ -1,35 +1,52 @@
 import * as React from "react";
-//import { useRef } from "react";
-//import { motion} from 'framer-motion';
-//import background from "../bg2.png";
-/* import backIcon from "../Icon/back.png"; */
-import Bback from "../Icon/back.png";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { Link } from 'react-router-dom';
+
 import '../App.css';
+import '../scroll.css';
+
+import Bback from "../Icon/back.png";
 import SetHeader from "../Img/WSetHeader.png";
 import SetBody from "../Img/SetBody.png";
-//import { Frame, Scroll } from "framer";
-import '../scroll.css';
-import { motion, useViewportScroll, useTransform } from "framer-motion";
-
-//new
-
-//import { motion, useViewportScroll, useTransform, useMotionValue } from "framer-motion";
-
 
 const ScrollTitle = () => {
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0]);
-//new
-/*   const { scrollYProgress } = useViewportScroll();
-const scale = useTransform(scrollYProgress, [0, 1], [0, 1]); */
-/* const y = useMotionValue(0)
-const cardy = useTransform(y, value => value /-1)
-const bgy = useTransform(y, value => value /2) */
+
+  const y = useMotionValue(0);
+  const yInput = [0, -960];
+  const fontSize = useTransform(y, yInput, ["26px", "0px"]);
   return (
-    <div >
-        <Link to="/List">
-        <img  src={Bback} alt=" " style={{
+    <div className="all" style={{
+    overflow:'scroll'
+    }}>
+      <motion.p className="SetP"
+            style={{
+              position: 'fixed',
+              top: 56,
+              left:20,
+              margin: 0,
+              fontSize
+            }}> 设置
+      </motion.p>
+      <motion.div className="MoveBackground"
+          drag="y"
+          dragConstraints={{ top: -960, bottom: 0 }}
+      //    dragElastic={0}
+     //     dragTransition={{ bounceStiffness: 176, bounceDamping: 26 }}
+          style={{
+            y,
+         
+            backgroundColor: '#ddd',
+            backgroundImage: `url(${SetBody})`,
+            top:148,
+            width: 375,
+            height: 1590,
+            position: 'absolute',
+            opacity: 1,
+            zIndex: -10
+          }}>
+        </motion.div>
+      <Link className="BackIcon" to="/List">
+        <img src={Bback} alt=" " style={{
           width: 30,
           position: 'fixed',
           top: 20,
@@ -38,128 +55,20 @@ const bgy = useTransform(y, value => value /2) */
           zIndex: '10000'
         }} />
       </Link>
-
-    <div className="HeadCard"
-          
-    
-          style={{    
-            backgroundColor: '#bbb',
-            backgroundImage: `url(${SetHeader})`, 
-            top:0, 
-            left:0,
-            width: 375,
-            height: 148,
-            position: 'fixed',
-        
-            opacity: 1,
-            zIndex: -1
-          }}>
-        </div>
-
-    <div className="wrapper">
-      <motion.div
-        className="scontainer"
+      <div className="HeadCard"
         style={{
-          scale
-        }}
-      >
-      
-      <p 
-style={{
-position:'absolute',
-//top:50,
-//left: 20,
-fontWeight: 'bold',
-fontSize:26,
-zIndex:100
-}}>设置</p> 
-
-      {/*   <motion.div
-          className="item"
-          style={{
-            scaleY: scrollYProgress
-          }}
-        > </motion.div> */}
-      </motion.div>
- {/*      <div className="SBackground"
-              style={{
-                backgroundColor: '#eee',
-                width: 375,
-                height: 812,
-                position: 'absolute',
-                top: 0,
-                left:0,
-                opacity: 1,
-                zIndex: -1000
-              }} ></div>  */}
+          backgroundColor: '#bbb',
+          backgroundImage: `url(${SetHeader})`,
+          top: 0,
+          left: 0,
+          width: 375,
+          height: 148,
+          position: 'fixed',
+          opacity: 1,
+          zIndex: -1
+        }}>
+      </div>
     </div>
-
-
-
-
-          <motion.div className="MoveCard"
-          drag="y"
-          dragConstraints={{ top: -900, bottom: 60 }}
-            dragElastic={0}
-            dragTransition={{ bounceStiffness: 176, bounceDamping: 26 }}
-            style={{    
-              backgroundColor: '#ddd',
-              backgroundImage: `url(${SetBody})`, 
-              top:148, 
-              width: 375,
-              height: 1590,
-              position: 'absolute',
-              opacity: 1,
-              zIndex: -200
-            }}>
-          </motion.div>
-  {/*  <Scroll 
-      style={{
-    width:70,
-     height: 200,
-     backgroundColor: '#FFFFFF',
-     zIndex:700
-     }}
-     >
-
-      <Frame
-   style={{
-    width:70,
-     height: 70,
-     backgroundColor: '#000000',
-     zIndex:800
-   }}/>
-         <Frame
-   style={{
-    width:70,
-     height: 70,
-     backgroundColor: '#000000',
-     zIndex:800
-   }}/>
-         <Frame
-   style={{
-    width:70,
-     height: 70,
-     backgroundColor: '#000000',
-     zIndex:800
-   }}/>
-   </Scroll> */}
-
-     {/* ß */}
-          
-           {/*            <div className="Background"
-              style={{
-                backgroundColor: '#000',
-                width: 375,
-                height: 812,
-                position: 'absolute',
-                top: 0,
-                left:0,
-                opacity: 1,
-                zIndex: -1000
-              }} ></div> */}
-          </div>
-
   );
 }
 
