fb6c1e6148c95817bee9765fb3044b4c523d98bf
JackieLee <nyjackielee@163.com>
2021-10-11 17:32:02 +0800

    数字反馈完成

--- src/ClickFold/ClickNum.js
+++ src/ClickFold/ClickNum.js
@@ -3,8 +3,37 @@ import { motion } from 'framer-motion';
 import Bback from "../Icon/back.png";
 import { Link } from 'react-router-dom';
 import ClickN from "../Img/ClickN.png";
+import { Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 
+const GridBox = {
+  width: 315,
+  height: 256,
+  //backgroundColor: "blue",
+  position: "absolute",
+  bottom: 72,
+  left: 30,
+  paddingRight: 0,
+  display: "flex"
+}
+const RowGridBox = {
+  width: 100,
+  marginLeft: 23,
+  marginRight: 24,
+//  backgroundColor: 'red',
+}
+const ColGridBox = {
+  marginTop: 3,
+  height: 58,
+  borderRadius: 35,
+//  backgroundColor: '#000000',
+
+}
+const VBoxTap = {
+ // scale: 0.9,
+  backgroundColor: '#000000',
+  opacity: 0.1
+}
 const ClickNum = () => {
   return (
     <div >
@@ -18,7 +47,27 @@ const ClickNum = () => {
           zIndex: '100'
         }} />
       </Link>
+      <div className="R" style={GridBox}>
 
+        <div className="C" style={RowGridBox}>
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+        </div>
+        <div className="C" style={RowGridBox}>
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+        </div>
+        <div className="C" style={RowGridBox}>
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+          <motion.div className="C" whileTap={VBoxTap} style={ColGridBox} />
+        </div>
+      </div>
       <div className="Background"
         style={{
           backgroundImage: `url(${ClickN})`,
