64a9d3f225f33b01019ea7b8fba10fce0150cda2
JackieLee <nyjackielee@163.com>
2021-10-19 11:06:55 +0800

    finish window1

--- src/WindowFold/WindowBottom.js
+++ src/WindowFold/WindowBottom.js
@@ -1,38 +1,103 @@
-import  React from "react";
+import { motion, useCycle, AnimatePresence } from 'framer-motion';
+import * as React from "react";
 import { Link } from 'react-router-dom';
 import backIcon from "../Icon/back.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ContactBtnStyle =  {                          
+    position: 'relative',
+    
+    bottom: -750,
+    width: 60,
+    height: 60,
+    backgroundColor: "#aaa",
+    borderRadius: 20,
+    textAlign: 'center',
+    zIndex:1,opacity:0.5,
+    display:'flex',
+    justifyContent: 'center',
+}
+const ContactBubbleStyle = {
+  width: 300,
+        height: 300,
+    boxShadow: "0px 0px 4px 0px rgba(0, 0, 0, 0.05)",
+    opacity: 1,
+    background: '#666',
+    borderRadius: 12,
+    position: 'absolute',
+  
+    bottom: 100,
+    zIndex: 20
+}
+const BoxAnimation = {
+    animationOne: {
+      y:0
+    
+    },
+    animationTwo: {
+      y:-200
+ 
+    }
+}
+const BackIconStyle ={
+    width: 30,
+    position: 'fixed',
+    top: 20,
+    left: 20,
+    opacity: 1,
+    zIndex: '100'
+ }
+
+const ContactBodyStyle ={
+    backgroundColor: '#EEEEEE',
+    position: 'absolute',
+    width: 375,
+    height: 812,
+    zIndex: 1,
+ }
+
+
 
 const WindowBottom = () => {
-  return (
-    <div 
-    style={{
-    display: 'flex',
-    justifyContent: 'center'
-    }}>
-      <Link to="/Window">
-        <img src={backIcon} alt=" " style={{
-          width: 30,
-          position: 'absolute',
-          top: 20,
-          left: 20,
-          opacity: 1,
-          zIndex: '100'
+    const [animationBox, cycleAnimation] = useCycle("animationOne", "animationTwo");
+
+    return (
+        <div className="All"
+        style={{
+            width: '100%',
+       display: 'flex',
+       justifyContent: 'center',
+        height: 812,
+        position: 'absolute'
+      
         }}>
-        </img>
-      </Link>
-      <div className="bg"
-          style={{
-            backgroundColor: "#eee",
-            width: 375,
-            height: 812,
-            position: 'absolute',
-            top: 0,
-            opacity: 1,
-            zIndex: '2'
-          }}
-        ></div>
-    </div>
-  );
+            <Link to="/Window">
+                <img src={backIcon} 
+                alt=" " 
+                style={BackIconStyle}
+                />         
+            </Link>
+
+    
+                <div className='ContactBody'
+                    style={ContactBodyStyle}
+                />
+            <AnimatePresence>
+                    <div className='ContactBtn' 
+                    onClick={() => cycleAnimation()} 
+                    style={ContactBtnStyle}
+                 >
+                       <motion.div className='boxChange'
+                    style={ContactBubbleStyle}
+                    variants={BoxAnimation}
+                    animate={animationBox}
+                /></div>
+                    
+             
+            </AnimatePresence>
+        </div>
+    );
 }
 
 export default WindowBottom;
+
