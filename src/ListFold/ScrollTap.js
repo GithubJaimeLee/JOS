8816a682412f80923c05a1badd301c53354b1785
JackieLee <nyjackielee@163.com>
2021-10-30 13:19:55 +0800

    add tap

--- src/ListFold/ScrollTap.js
+++ src/ListFold/ScrollTap.js
@@ -66,6 +66,22 @@ const ScrollTap = () => {
               position: "absolute",
             }}
           />
+          <ul>
+            <li>
+              <a class="active" href="#home">
+                主页
+              </a>
+            </li>
+            <li>
+              <a href="#news">新闻</a>
+            </li>
+            <li>
+              <a href="#contact">联系</a>
+            </li>
+            <li>
+              <a href="#about">关于</a>
+            </li>
+          </ul>
           {/*         <div
         style={{
         width:66,
