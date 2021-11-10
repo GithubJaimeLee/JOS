bd948fb08b5054377b45f6c05978191afc3cd2ae
JackieLee <nyjackielee@163.com>
2021-11-10 20:15:38 +0800

    FIX Delete img

--- src/DeleteFold/DeleteImgApp.jsx
+++ src/DeleteFold/DeleteImgApp.jsx
@@ -132,6 +132,7 @@ const UploadGallery = () => {
         collisionDetection={closestCorners}
         onDragStart={handleDragStart}
         // 下面两项是重点
+        // whileTap={handleDragOver}
         onDragOver={handleDragOver}
         onDragEnd={handleDragEnd}
         // 上面两项是重点
@@ -202,11 +203,11 @@ const UploadGallery = () => {
 
   function handleDragOver(event) {
     const { active, over } = event;
-
     if (active.id !== over.id) {
       setItems((items) => {
         const oldIndex = items.indexOf(active.id);
         const newIndex = items.indexOf(over.id);
+
         return arrayMove(items, oldIndex, newIndex);
       });
     }
