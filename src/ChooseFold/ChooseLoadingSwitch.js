fa26d472f8bb852e10aa92767d5b379ab717ccfc
JackieLee <nyjackielee@163.com>
2021-11-17 19:54:01 +0800

    fix switch

--- src/ChooseFold/ChooseLoadingSwitch.js
+++ src/ChooseFold/ChooseLoadingSwitch.js
@@ -1,10 +1,47 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { useState } from "react";
+import { Switch } from "antd";
 import NavBarPage from "../Component/NavBarPage";
 import Bg from "../Component/Bg";
+import { motion, useCycle } from "framer-motion";
 
 const ChooseLoadingSwitch = () => {
   const Info = <p>单选框</p>;
+  const [isOn, setIsOn] = useState(false);
+  const toggleSwitch = () => setIsOn(!isOn);
+  const BtnVariants = {
+    animationOne: {
+      justifyContent: "flex-end",
+      backgroundColor: "#5F7DF7",
+    },
+    animationTwo: {
+      justifyContent: "flex-start",
+      backgroundColor: "#ddd",
+    },
+  };
+  const PointVariants = {
+    animationOne: {
+      height: 20,
+      width: 8,
+      borderRadius: "4px",
+      backgroundColor: "#fff",
+    },
+    animationTwo: {
+      backgroundColor: "#b2b2b2",
+      height: 16,
+      width: 16,
+      borderRadius: "16px",
+    },
+  };
+  const [BtnAnimation, cycleAnimation] = useCycle(
+    "animationOne",
+    "animationTwo"
+  );
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
   return (
     <div className="All">
       <NavBarPage placement={"end"} contextTitle={"说明"} context={Info} />
@@ -21,39 +58,47 @@ const ChooseLoadingSwitch = () => {
         <div
           className="bg"
           style={{
-            backgroundColor: "#eee",
             width: 375,
             height: 812,
             position: "absolute",
             top: 0,
+            backgroundColor: "#E6ECF4",
             display: "grid",
             justifyContent: "center",
+            alignContent: "space-evenly",
             opacity: 1,
             zIndex: "2",
           }}
         >
-          <Form
+          <motion.div
             style={{
-              position: "relative",
-              height: 60,
-              top: 300,
-              left: 0,
-              zIndex: "2",
+              width: 60,
+              height: 30,
+              backgroundColor: "rgba(19, 3, 3, 0.4)",
+              display: "flex",
+              alignItems: "center",
+              borderRadius: 50,
+              padding: 10,
+              // cursor: "pointer",
             }}
+            className="Switch"
+            data-isOn={isOn}
+            variants={BtnVariants}
+            animate={BtnAnimation}
+            transition={spring}
+            onClick={(toggleSwitch, cycleAnimation)}
           >
-            <Form.Check type="switch" id="custom-switch" label="开关" />
-          </Form>
-          <Form
-            style={{
-              position: "relative",
-              height: 60,
-              //  top: 200,
-              left: 0,
-              zIndex: "2",
-            }}
-          >
-            <Form.Check type="switch" id="custom-switch" label="开关" />
-          </Form>
+            <motion.div
+              style={{
+                position: "relative",
+              }}
+              variants={PointVariants}
+              animate={BtnAnimation}
+              className="Handle"
+              transition={spring}
+            />
+          </motion.div>
+          <Switch defaultChecked />
         </div>
       </div>
       <Bg />
